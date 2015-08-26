0fdc67e30cce0e6837025f81419fb10a6eb53f4b
Andrew Cutler <macropin@gmail.com>
2015-08-26 14:37:44 +1000

    Better escaping of detault vars

--- entry.sh
+++ entry.sh
@@ -11,30 +11,30 @@ genpasswd() {
 
 
 # MySQL Service
-export DB_DSNW="mysql://${DATABASE_USER-roundcube}:${DATABASE_PASS}@${DATABASE_HOST-$MARIADB_PORT_3306_TCP_ADDR}/${DATABASE_NAME-roundcube}"
+export DB_DSNW="mysql://${DATABASE_USER:-roundcube}:${DATABASE_PASS}@${DATABASE_HOST:-$MARIADB_PORT_3306_TCP_ADDR}/${DATABASE_NAME:-roundcube}"
 
 # IMAP Service
 export DEFAULT_HOST="${DEFAULT_HOST-ssl://${MAILSERVER_PORT_993_TCP_ADDR}:$MAILSERVER_PORT_993_TCP_PORT}"
-export DEFAULT_PORT="${DEFAULT_PORT-$MAILSERVER_PORT_993_TCP_PORT}"
+export DEFAULT_PORT="${DEFAULT_PORT:-$MAILSERVER_PORT_993_TCP_PORT}"
 
 # SMTP Service
-export SMTP_SERVER="${SMTP_SERVER-ssl://${MAILSERVER_PORT_465_TCP_ADDR}:$MAILSERVER_PORT_465_TCP_PORT}"
-export SMTP_PORT="${SMTP_PORT-$MAILSERVER_PORT_465_TCP_PORT}"
+export SMTP_SERVER="${SMTP_SERVER:-ssl://${MAILSERVER_PORT_465_TCP_ADDR}:$MAILSERVER_PORT_465_TCP_PORT}"
+export SMTP_PORT="${SMTP_PORT:-$MAILSERVER_PORT_465_TCP_PORT}"
 
 export DES_KEY=$(genpasswd 24)
 
 if [ "$SSL_ENABLED" == "true" ]; then
     a2enmod ssl
-    export SSL_KEY=${SSL_KEY-/etc/ssl/certs/ssl-cert-snakeoil.pem}
-    export SSL_CRT=${SSL_CRT-/etc/ssl/private/ssl-cert-snakeoil.key}
-    export SSL_CA=${SSL_CA-/etc/ssl/certs/ca-certificates.crt}
+    export SSL_KEY=${SSL_KEY:-/etc/ssl/certs/ssl-cert-snakeoil.pem}
+    export SSL_CRT=${SSL_CRT:-/etc/ssl/private/ssl-cert-snakeoil.key}
+    export SSL_CA=${SSL_CA:-/etc/ssl/certs/ca-certificates.crt}
 fi
 
 # Apache MPM Tuning
-export MPM_START=${MPM_START-5}
-export MPM_MINSPARE=${MPM_MINSPARE-5}
-export MPM_MAXSPARE=${MPM_MAXSPARE-10}
-export MPM_MAXWORKERS=${MPM_MAXWORKERS-150}
-export MPM_MAXCONNECTIONS=${MPM_CONNECTIONS-0}
+export MPM_START=${MPM_START:-5}
+export MPM_MINSPARE=${MPM_MINSPARE:-5}
+export MPM_MAXSPARE=${MPM_MAXSPARE:-10}
+export MPM_MAXWORKERS=${MPM_MAXWORKERS:-150}
+export MPM_MAXCONNECTIONS=${MPM_CONNECTIONS:-0}
 
 exec $@
